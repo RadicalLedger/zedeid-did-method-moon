0020ad522959b03b2da237a2802c5b5517e6b3db
Terance Edmonds <teranceedmonds20@gmail.com>
2024-05-07 08:32:25 +0530

    verification method document

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -20,29 +20,36 @@ describe('HD Wallet Moon Method', function () {
             publicKey: '03844a5d329470697de9926c9c98839ea33b6dd9507a896194ae2b91d71faa16d6',
             chainCode: 'dac0c414d5006b7350e3b7750e5b535af7ecd9b5a2ad00648d427349885f4358',
             didDocument: {
-                '@context': 'https://w3id.org/did/v1',
+                '@context': [
+                    'https://www.w3.org/ns/did/v1',
+                    'https://w3id.org/security/suites/secp256k1recovery-2020/v2',
+                    'https://w3id.org/security/v3-unstable'
+                ],
                 id: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407',
-                publicKey: [
+                verificationMethod: [
                     {
-                        id: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#owner',
-                        owner: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407',
-                        type: 'Secp256k1VerificationKey2018',
-                        ethereumAddress: '0x022b971dff0c43305e691ded7a14367af19d6407'
+                        id: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#controller',
+                        controller: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407',
+                        type: 'EcdsaSecp256k1RecoveryMethod2020',
+                        blockchainAccountId:
+                            'eip155:1287:0x022b971dff0c43305e691ded7a14367af19d6407'
+                    },
+                    {
+                        id: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#delegate-1',
+                        controller: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407',
+                        type: 'EcdsaSecp256k1VerificationKey2019',
+                        publicKeyHex:
+                            '03844a5d329470697de9926c9c98839ea33b6dd9507a896194ae2b91d71faa16d6'
                     }
                 ],
                 authentication: [
-                    {
-                        type: 'Secp256k1SignatureAuthentication2018',
-                        publicKey: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#owner'
-                    }
+                    'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#controller',
+                    'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#delegate-1'
                 ],
                 assertionMethod: [
-                    {
-                        type: 'Secp256k1SignatureAuthentication2018',
-                        publicKey: 'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#owner'
-                    }
-                ],
-                service: []
+                    'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#controller',
+                    'did:moon:0x022b971dff0c43305e691ded7a14367af19d6407#delegate-1'
+                ]
             }
         });
     });
